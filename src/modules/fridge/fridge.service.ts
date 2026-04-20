2534b75b234d15e1371d911acbab733c0ad0a1ce
Aphixyz <apdbnz02@gmail.com>
2026-04-20 12:22:03 +0700

    make fridge-product

--- src/modules/fridge/fridge.service.ts
+++ src/modules/fridge/fridge.service.ts
@@ -8,14 +8,48 @@ export interface Fridge {
   [key: string]: any;
 }
 
-export const fridgeService = {
-  findFridgeDetailByHomeId: async (homeId: number) => {
-    const fridgeDetails = await fridgeRepository.findFridgeDetailByHomeId(homeId);
+export interface Product {
+  id: number;
+  name: string;
+  category_id: number;
+  categories_name: string;
+  quantity: number;
+  unit: string;
+  expiry_date: string;
+  status: string;
+  [key: string]: any;
+}
+const statusMap: Record<string, string> = {
+  "Active": "ปกติ",         // หรือจะใช้คำว่า "พร้อมทาน", "ใช้งานอยู่"
+  "Expired": "หมดอายุ",
+  "Expiring": "ใกล้หมดอายุ", // ถ้ามี
+  "Consumed": "หมดแล้ว"     // ถ้ามี
+};
 
+export const fridgeService = {
+
+  async findFridgeDetailByHomeId(homeId: number) {
+    const fridgeDetails = await fridgeRepository.findFridgeDetailByHomeId(homeId);
     if (fridgeDetails.length === 0) {
       throw appError.notFound('Fridge not found');
     }
-
     return fridgeDetails;
   },
+
+  async findProductsByFridgeId(fridgeId: number) {
+    const products = await fridgeRepository.FindProductsByFridgeId(fridgeId);
+    if (products.length === 0) {
+      throw appError.notFound('Products not found');
+    }
+    return this.mapProductList(products);
+  },
+
+  mapProductList(products: Product[]) {
+    return products.map((product) => ({
+      ...product,
+      status: statusMap[product.status] || product.status
+    }));
+  },
+
+
 };
